fb9b943851710531691d61e1a322ffcba5b492ba
Moses Boone <moses.s.boone@gmail.com>
2019-03-07 21:56:15 -0800

    Creating + Updating books functional

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Books = require('../models').Books;
+const sequelize = require('sequelize');
 
 
 // redirect to books from root
@@ -10,46 +11,62 @@ router.get('/', function(req, res) {
 
 // GET - main books page 
 router.get('/books', function (req, res) {
-  Books.findAll().then((result) => {
+  Books.findAll({
+    order: sequelize.literal('year DESC')
+  }).then((result) => {
     res.render('index', { books: result });
   }).catch((err) => {
-    
+    console.log(err);
   });
 });
 
-// GET -  new book form
-router.get('/books/new', function (req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-// POST - new book form
-
-// detailed book router
-router.route('/books/:id')
-  .all((req, res, next) => {
-    /* instead of finding the book on both the get and post, 
-    find it once and pass it through middleware */
-    Books.findByPk(req.params.id).then((result) => {
-      res.locals.book = result;
-      next();
-    });
+// routing wrapper for new books
+router.route('/books/new') 
+  // GET -  new book form
+  .get((req, res) => {
+    res.render('new-book');
   })
-  // GET - book details
-  .get(function (req, res) {
-    res.render('update-book', {target: `/books/${req.params.id}`});
-  })
-  // POST - update book details
+  // POST - new book form
   .post((req, res) => {
-    res.locals.book.update(req.body).then((result) => {
+    Books.create(req.body).then((result) => {
       res.redirect(`/books/${result.id}`);
     }).catch((err) => {
       console.log(err);
     });
   });
 
+
+// middleware to find the book by id
+router.use('/books/:id', (req, res, next) => {
+  /* instead of finding the book on 3 different routes, 
+  find it once and pass it through middleware */
+  Books.findByPk(req.params.id).then((result) => {
+    res.locals.book = result;
+    next();
+  });
+});
+
+// GET - book details
+router.get('/books/:id', (req, res) => {
+  res.render('update-book', {target: `/books/${req.params.id}`});
+});
+
+// POST - update book details
+router.post('/books/:id', (req, res) => {
+  res.locals.book.update(req.body).then(() => {
+    res.redirect('/books');
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
 // POST - delete book
-router.get('/books/:id/delete', function (req, res) {
-  res.render('index', { title: 'Express' });
+router.post('/books/:id/delete', function (req, res) {
+  res.locals.book.destroy().then(() => {
+    res.redirect('/books');
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 
 // export routing handler
