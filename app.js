18a931cb94c70d6ab47b45bd42cfe8b929bdca81
Moses Boone <moses.s.boone@gmail.com>
2019-03-07 21:19:50 -0800

    Update form functional

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 // initializing the app
 const app = express();
@@ -13,27 +14,27 @@ app.set('view engine', 'pug');
 
 // middleware
 app.use(logger('dev'));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // --------- routing -----------
 // main routing module
 const router = require('./routes/index');
-app.use(router);
+app.use('/', router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals
-  res.locals.error = err;
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals
+//   res.locals.error = err;
   
-  // render the error page
-  res.render('error');
-});
+//   // render the error page
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Books = require('../models').Books;
 
+
 // redirect to books from root
 router.get('/', function(req, res) {
   res.redirect('/books');
@@ -23,17 +24,33 @@ router.get('/books/new', function (req, res) {
 
 // POST - new book form
 
-// GET - book details
-router.get('/books/:id', function (req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-// POST - update book details
+// detailed book router
+router.route('/books/:id')
+  .all((req, res, next) => {
+    /* instead of finding the book on both the get and post, 
+    find it once and pass it through middleware */
+    Books.findByPk(req.params.id).then((result) => {
+      res.locals.book = result;
+      next();
+    });
+  })
+  // GET - book details
+  .get(function (req, res) {
+    res.render('update-book', {target: `/books/${req.params.id}`});
+  })
+  // POST - update book details
+  .post((req, res) => {
+    res.locals.book.update(req.body).then((result) => {
+      res.redirect(`/books/${result.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
 
 // POST - delete book
 router.get('/books/:id/delete', function (req, res) {
   res.render('index', { title: 'Express' });
 });
 
-// export routing object
+// export routing handler
 module.exports = router;
